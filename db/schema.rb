8c2362b3d73830f079da3e7841acd4d0e7b28546
hiroya <hiroya19890721@gmail.com>
2021-03-21 23:34:36 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_140307) do
+ActiveRecord::Schema.define(version: 2021_03_21_142308) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
@@ -20,5 +28,6 @@ ActiveRecord::Schema.define(version: 2021_03_21_140307) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
