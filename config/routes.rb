aa6a2b6242c00e33aff3c6b522125ffd9d7472db
hiroya <hiroya19890721@gmail.com>
2021-03-22 23:02:34 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :user,
+  controller: {registrations: 'registrations'}
   root 'pages#home'
+  get '/users/:id', to: 'users#show', as:'user'
+
+
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create',as:'post_photos'
 end
