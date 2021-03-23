51b0b0b7445df4f4b20d117bde8d90d0e58a03a8
hiroya <hiroya19890721@gmail.com>
2021-03-23 21:10:27 +0900

    devisスタイル変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?:action_controller?:action_controller
+
+  protected
+
+  def configure_permitted_paramenters
+    devise_paramenter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_paramenter_sanitizer.permit(:account_update, kyes: [:name])
+  end
 end
