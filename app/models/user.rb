e85011b2348255c449057ebc2517017be709cfa9
hiroya <hiroya19890721@gmail.com>
2021-03-23 20:57:53 +0900

    マイグレーションファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
+
+  validates :name,presence: true,length: { maximum: 50 }
 end
