28d18f4912aab0418764e141468f33da1a372550
hiroya <hiroya19890721@gmail.com>
2021-03-23 19:56:39 +0900

    devise_user rails db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  has_many :posts,dependent: :destroy
+  # has_many :posts,dependent: :destroy
 
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
+end
