2bdbb10a8de34b7965fc8a57a56dadabf7ff1179
hiroya <hiroya19890721@gmail.com>
2021-03-25 04:31:44 +0900

    サインイン画面(new.html)スタイル変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?:action_controller?:action_controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_paramenters
-    devise_paramenter_sanitizer.permit(:sign_up, keys:[:name])
-    devise_paramenter_sanitizer.permit(:account_update, kyes: [:name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_234441) do
+ActiveRecord::Schema.define(version: 2021_03_23_113050) do
 
   create_table "photos", force: :cascade do |t|
     t.string "image", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_234441) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
