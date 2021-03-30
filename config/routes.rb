7f9ec412ab56c9fa4a9f959ae529b9b332044294
hiroya <hiroya19890721@gmail.com>
2021-03-30 23:09:10 +0900

    topページ変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users,
   controller: {registrations: 'registrations'}
-  root 'pages#home'
+  root 'posts#index'
   get '/users/:id', to: 'users#show', as:'user'
 
 
