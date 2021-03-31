326981f5dff277bb356fb7add4d7aa5b43265009
hiroya <hiroya19890721@gmail.com>
2021-04-01 08:50:56 +0900

    posts_controller set_postメソッド追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,23 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.user == current_user
+      flash[:notice] = "投稿削除しました"
+    else 
+      flash[:alert] = "投稿の削除に失敗しました"
+  
+    end
+    redirect_to root_path
+  end
+
   private 
   def post_params
     params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
   end
 end
